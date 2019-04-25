940bec4c160fd053b2a5424beef779ef27dd5fef
Elbaruni <mohamed.elbaruni@gmail.com>
2019-04-25 15:18:07 +0200

    completed project

---
+++ truffle-config.js
@@ -0,0 +1,33 @@
+//require('dotenv').config();
+const HDWalletProvider = require('truffle-hdwallet-provider');
+
+module.exports = {
+
+  networks: {
+    rinkeby: {
+     
+
+      provider: function () {
+        return new HDWalletProvider("body sell knock seek sign lend finish push fatal grab lazy dismiss", "https://rinkeby.infura.io/v3/2a3ef5517fce4991b5d2c4cf004ff40d")
+      },
+      gasPrice: 44000000000,
+      network_id: 4
+      
+    },
+    development: {
+      host: "127.0.0.1",
+      port: "7545",
+      gas: 4600000,
+      network_id: "*"
+    }
+  },
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200
+    }
+  }
+
+
+
+}
